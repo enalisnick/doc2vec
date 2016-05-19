c7b4fc39802d6b48b55ec29697795b07c1b559f1
enalisnick <enalisnick@gmail.com>
2016-05-18 17:34:30 -0700

    runs on IMDB

--- testRun.cpp
+++ testRun.cpp
@@ -3,9 +3,9 @@
 
 int main(int argc, char **argv) {
   Doc2Vec doc2vec;
-  doc2vec.train("/data/paper.title.seg", 50, 0, 1, 0, 15, 10, 0.025, 1e-5, 3, 6);
-  printf("\nWrite model to %s\n", "/data/model.title.sg");
-  FILE * fout = fopen("/data/model.title.sg", "wb");
+  doc2vec.train("data/aclImdb_for_doc2vec.txt", 50, 0, 1, 0, 15, 10, 0.025, 1e-5, 3, 6);
+  printf("\nWrite model to %s\n", "data/imdb_large.model");
+  FILE * fout = fopen("data/imdb_large.model", "wb");
   doc2vec.save(fout);
   fclose(fout);
   return 0;
